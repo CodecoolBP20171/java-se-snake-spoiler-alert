65626f090a6f755d4994377d47c4e64481c7b34a
Péter Biró <biro.peter.hu@gmail.com>
2017-09-12 11:17:50 +0200

    blank Battery power up
    misses the laser charging part, only new power up show up on pane

---
+++ src/com/codecool/snake/entities/powerups/Battery.java
@@ -0,0 +1,32 @@
+package com.codecool.snake.entities.powerups;
+
+import com.codecool.snake.entities.GameEntity;
+import com.codecool.snake.Globals;
+import com.codecool.snake.entities.Interactable;
+import com.codecool.snake.entities.snakes.SnakeHead;
+import javafx.scene.layout.Pane;
+
+import java.util.Random;
+
+public class Battery extends GameEntity implements Interactable {
+
+    public Battery(Pane pane) {
+        super(pane);
+        setImage(Globals.powerupBattery);
+        pane.getChildren().add(this);
+
+        Random rnd = new Random();
+        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
+        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
+    }
+
+    @Override
+    public void apply(SnakeHead snakeHead) {
+        destroy();
+    }
+
+    @Override
+    public String getMessage() {
+        return "Got laser charge :)";
+    }
+}
