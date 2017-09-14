a949dd4f84009d85edde554221f869bdf43f43db
Zsofia Radai <zsofiaradai@gmail.com>
2017-09-14 13:27:03 +0200

    menu shown, exit works

--- src/com/codecool/snake/Main.java
+++ src/com/codecool/snake/Main.java
@@ -1,7 +1,12 @@
 package com.codecool.snake;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,11 +18,26 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         Game game = new Game();
-
         primaryStage.setTitle("Snake Game");
-        primaryStage.setScene(new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT));
+        Scene scene = new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT);
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
+        game.getChildren().add(menuBar);
+
+        Menu fileMenu = new Menu("File");
+        MenuItem newGameItem = new MenuItem("New game");
+        MenuItem helpMenuItem = new MenuItem("Help");
+        MenuItem exitMenuItem = new MenuItem("Exit");
+        fileMenu.getItems().add(newGameItem);
+        fileMenu.getItems().add(helpMenuItem);
+        fileMenu.getItems().add(exitMenuItem);
+        exitMenuItem.setOnAction(actionEvent -> Platform.exit());
+        newGameItem.setOnAction(actionEvent -> game.start());
+
+        menuBar.getMenus().addAll(fileMenu);
+        primaryStage.setScene(scene);
         primaryStage.show();
-        game.start();
     }
 
 }
