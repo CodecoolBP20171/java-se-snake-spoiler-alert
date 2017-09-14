0eca481c22dc8b3fdef95481c7b784bb539d8c75
akosENagy <akos.e.nagy@gmail.com>
2017-09-14 12:04:06 +0200

    Added abstract Enemy class and revised SimpleEnemy to inherit off of it

---
+++ src/com/codecool/snake/entities/enemies/Enemy.java
@@ -0,0 +1,35 @@
+package com.codecool.snake.entities.enemies;
+
+import com.codecool.snake.entities.GameEntity;
+import com.codecool.snake.Globals;
+import com.codecool.snake.Utils;
+import com.codecool.snake.entities.snakes.SnakeHead;
+import javafx.geometry.Point2D;
+import javafx.scene.layout.Pane;
+
+import java.util.Random;
+
+public abstract class Enemy extends GameEntity {
+
+    protected Point2D heading;
+    protected static final int damage = 10;
+    protected static final int speed = 1;
+    //int spawnrate?
+
+    public Enemy(Pane pane) {
+        super(pane);
+        pane.getChildren().add(this);
+
+        //make sure they don't spawn on the player
+        Random rnd = new Random();
+        SnakeHead player = Globals.snakeHeadEntity;
+        do {
+            setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
+            setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
+        } while (getBoundsInParent().intersects(player.getBoundsInParent()));
+
+        double direction = rnd.nextDouble() * 360;
+        setRotate(direction);
+        heading = Utils.directionToVector(direction, speed);
+    }
+}
