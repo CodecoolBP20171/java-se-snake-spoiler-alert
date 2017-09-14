016bcd97c412e0cfb92fbf8085bf2e757b192a81
akosENagy <akos.e.nagy@gmail.com>
2017-09-14 12:17:36 +0200

    Applied abstract class inheritance to follower and babyface enemies

--- src/com/codecool/snake/entities/enemies/Enemy.java
+++ src/com/codecool/snake/entities/enemies/Enemy.java
@@ -1,19 +1,22 @@
 package com.codecool.snake.entities.enemies;
 
+import com.codecool.snake.entities.Animatable;
 import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.Globals;
 import com.codecool.snake.Utils;
+import com.codecool.snake.entities.Interactable;
 import com.codecool.snake.entities.snakes.SnakeHead;
 import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
 
 import java.util.Random;
 
-public abstract class Enemy extends GameEntity {
+public abstract class Enemy extends GameEntity implements Animatable, Interactable {
 
     protected Point2D heading;
     protected static final int damage = 10;
     protected static final int speed = 1;
+    protected double direction;
     //int spawnrate?
 
     public Enemy(Pane pane) {
@@ -28,7 +31,7 @@ public abstract class Enemy extends GameEntity {
             setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
         } while (getBoundsInParent().intersects(player.getBoundsInParent()));
 
-        double direction = rnd.nextDouble() * 360;
+        direction = rnd.nextDouble() * 360;
         setRotate(direction);
         heading = Utils.directionToVector(direction, speed);
     }
